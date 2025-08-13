2f01244d9d71444ad932902ee9b75608dd347956
jungin <changju@bu.edu>
2025-08-13 08:14:38 -0400

    Remove ignored files

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -24,7 +24,7 @@ const Location: React.FC = () => {
             <div className="w-full h-[400px]">
                 <iframe
                     title="학원 위치"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.017424328194!2d128.5430277152419!3d35.82703408016071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3565e3ad6b17d33f%3A0x74b879da3d4ed11e!2z64yA6rWs6rSR7JetIOqzoOyCrOqzteyCrOumrOyasOyLnCDshLHrj5kg7IKw7Yq47JWE7J2065SU7Ja064-ZIDUtMQ!5e0!3m2!1sko!2skr!4v1691904695701!5m2!1sko!2skr"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3235.258955244861!2d128.51233269767087!3d35.818121728723284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3565fadd55af3461%3A0x7accf2928e472abf!2s5%EC%B8%B5%2C%2020-4%20Joamnam-ro%2032-gil%2C%20Dalseo%20District%2C%20Daegu!5e0!3m2!1sen!2skr!4v1755087167135!5m2!1sen!2skr"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
